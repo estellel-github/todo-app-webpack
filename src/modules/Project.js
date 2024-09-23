ffd6bf7d52fa486310e52456b2565fd1251e6cc9
estellel-github <131867732+estellel-github@users.noreply.github.com>
2024-09-23 16:22:47 +0200

    Move task array from Project to Taskmanager

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -2,7 +2,6 @@ class Project {
   constructor(id, name) {
     this._id = id;
     this._name = name;
-    this._tasks = [];
   }
 
   get id() {
@@ -21,21 +20,6 @@ class Project {
     this._name = value;
   }
 
-  get tasks() {
-    return this._tasks;
-  }
-
-  set tasks(tasks) {
-    this._tasks = tasks;
-  }
 }
 
 export { Project };
-
-// const project1 = new Project("Test Project Title");
-
-// const task1 = new Task("1", "Work Project", "Active", "Test title", "Test desc", "2023-09-15", "High", "Meeting notes", ["Item 1", "Item 2"]);
-
-// project1.addTask(task1);
-
-// console.table(project1);
