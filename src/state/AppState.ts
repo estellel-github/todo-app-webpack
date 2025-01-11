b435abf5869b19c27041de7d871ae117a3cd0f60
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-11 16:00:26 +0100

    Update state logic -- Fix createStore syntax -- Add logic to trigger update -- Add logic to subscribe to state change

--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -1,36 +1,41 @@
-import create from 'zustand';
+import { createStore } from 'zustand/vanilla'; // Changed from 'zustand'
+import { Filter } from '../types/AppTypes';
 
 type AppState = {
   activeProjectId: number;
   currentViewType: 'project' | 'filter';
-  currentFilter: string;
+  currentFilter: Filter;
   isModalOpen: boolean;
   isTaskPaneOpen: boolean;
   activeTaskId: number | null;
   isNewTaskButtonVisible: boolean;
   isAddProjectVisible: boolean;
   editProjectId: number | null;
+  needsUpdate: boolean;
   setActiveProjectId: (id: number) => void;
   setViewType: (viewType: 'project' | 'filter') => void;
-  setFilter: (filter: string) => void;
+  setFilter: (filter: Filter) => void;
   toggleModal: (isOpen: boolean) => void;
   toggleTaskPane: (isOpen: boolean, taskId?: number | null) => void;
   setActiveTaskId: (taskId: number | null) => void;
   toggleNewTaskBtn: (isVisible: boolean) => void;
   toggleAddProject: (isVisible: boolean) => void;
   toggleEditProject: (projectId: number | null) => void;
+  triggerUpdate: () => void;
+  subscribeToStateChange: (callback: () => void) => void;
 };
 
-export const useAppState = create.createStore<AppState>((set) => ({
+export const useAppState = createStore<AppState>((set) => ({
   activeProjectId: 1,
   currentViewType: 'project',
-  currentFilter: '',
+  currentFilter: '📋 All Tasks',
   isModalOpen: false,
   isTaskPaneOpen: false,
   activeTaskId: null,
   isNewTaskButtonVisible: true,
   isAddProjectVisible: false,
   editProjectId: null,
+  needsUpdate: false,
   setActiveProjectId: (id) => set({ activeProjectId: id }),
   setViewType: (viewType) => set({ currentViewType: viewType }),
   setFilter: (filter) => set({ currentFilter: filter }),
@@ -41,4 +46,12 @@ export const useAppState = create.createStore<AppState>((set) => ({
   toggleNewTaskBtn: (isVisible) => set({ isNewTaskButtonVisible: isVisible }),
   toggleAddProject: (isVisible) => set({ isAddProjectVisible: isVisible }),
   toggleEditProject: (projectId) => set({ editProjectId: projectId }),
+  triggerUpdate: () =>
+    set((state) => ({
+      ...state,
+      needsUpdate: !state.needsUpdate,
+    })),
+  subscribeToStateChange: (callback: () => void) => {
+    useAppState.subscribe(callback);
+  }
 }));
