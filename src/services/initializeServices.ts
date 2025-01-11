abab995610ecb113d0ca4fc72a0a55ae890478a7
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-11 16:03:56 +0100

    Add service initialization -- (Optional: Seed local storage) -- Add list of tasks from local storage to TaskService -- Add list of projects from local storage to ProjectService

---
+++ src/services/initializeServices.ts
@@ -0,0 +1,47 @@
+import { projectService } from "../services/ProjectService";
+import { taskService } from "../services/TaskService";
+import { Task } from "../types/TaskTypes";
+import {
+  retrieveLocalTasks,
+  retrieveLocalProjects,
+} from "../services/LocalStorage";
+import { seedLocalStorage } from "../utils/seedLocalStorage"
+import { ProjectDetails, TaskDetails } from '../types/AppTypes';
+import { INBOX_ID } from "../utils/constants";
+
+
+// seedLocalStorage();
+
+const listProjects = () => {
+  projectService.addProject({ id: INBOX_ID, name: "🏠 Inbox" });
+  let storedProjectArray = retrieveLocalProjects();
+  if (storedProjectArray && storedProjectArray.length > 0) {
+    storedProjectArray.forEach((projectData: ProjectDetails) => {
+      projectService.addProject({ id: projectData.id, name: projectData.name });
+    });
+  }
+  console.table(projectService);
+};
+
+const listTasks = () => {
+  let storedTaskArray = retrieveLocalTasks();
+  storedTaskArray.forEach((taskData: TaskDetails) => {
+    const task = new Task({
+      id: taskData.id,
+      projectId: taskData.projectId,
+      status: taskData.status,
+      title: taskData.title,
+      dueDate: new Date(taskData.dueDate),
+      priority: taskData.priority,
+      notes: taskData.notes
+    }
+    );
+    taskService.addTask(task);
+  });
+  console.table(taskService.getAllTasks());
+};
+
+export const initializeServices = (): void => {
+  listProjects();
+  listTasks();
+}
