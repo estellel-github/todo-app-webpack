2a4b30e9c55526e1d2de2bd9b8ff9657b7b6568d
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-11 15:57:32 +0100

    Move status/prio to app types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,5 @@
 export const INBOX_ID = 1;
 
-export const TASK_STATUSES = ['To do', 'Done'] as const;
-
-export const TASK_PRIORITIES = ['Low', 'High'] as const;
-
 export const MESSAGES = {
   DELETE_TASK_CONFIRM: 'Are you sure you want to delete this task?',
   DELETE_PROJECT_CONFIRM: 'This project and all its tasks will be deleted permanently.',
