ed08575d6d5e6dd5b68f5d273a6541752b304911
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-14 18:08:03 +0100

    Update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const INBOX_ID = 1;
+export const INBOX_NAME = 'Inbox';
 
 export const MESSAGES = {
   DELETE_TASK_CONFIRM: '⚠️ Are you sure you want to delete this task?',
@@ -7,12 +8,29 @@ export const MESSAGES = {
   EMPTY_NAME_MSG: "⚠️ Please enter a project name",
 };
 
+export const DEFAULT_TXT = {
+  NEW_PROJECT_NAME: 'New Project Name',
+  NEW_TASK_TITLE: 'New Task',
+  DEFAULT_TASK_TITLE: 'Default Task Title',
+  NOTES_TXT: 'Add notes here...',
+  DONE_HEADER_CLOSED: '▶ Done',
+  DONE_HEADER_OPEN: '▼ Done'
+}
+
 export const APP_NAME: string = "☑️ To Do";
 
-export const MODAL_BUTTONS = {
+export const BUTTONS_TXT = {
+  ADD_PROJECT: '+ Add Project',
+  ADD_TASK: '+ Add Task',
   CONFIRM: 'Confirm',
+  SAVE: 'Save',
+  CREATE_TASK: 'Create task',
+  CREATE_PROJECT: 'Create project',
   CANCEL: 'Cancel',
   DELETE: 'Delete',
+  EDIT_ICON: '✏️',
+  SAVE_ICON: '💾',
+  DELETE_ICON: '🗑️'
 };
 
 export const TASKS_STORAGE_KEY: string = "TO DO App - Tasks";
