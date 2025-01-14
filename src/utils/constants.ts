cab47f9523d15132e4623b4078f512d6d3275064
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-01-14 11:30:41 +0100

    Add icons to messages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
 export const INBOX_ID = 1;
 
 export const MESSAGES = {
-  DELETE_TASK_CONFIRM: 'Are you sure you want to delete this task?',
-  DELETE_PROJECT_CONFIRM: 'This project and all its tasks will be deleted permanently.',
-  EMPTY_PROJECT_MSG: 'This project has no tasks. Add a new one!',
-  EMPTY_NAME_MSG: "Please enter a project name",
+  DELETE_TASK_CONFIRM: '⚠️ Are you sure you want to delete this task?',
+  DELETE_PROJECT_CONFIRM: '⚠️ This project and all its tasks will be deleted permanently.',
+  EMPTY_PROJECT_MSG: 'ℹ️ This project has no pending tasks. Go touch some grass!',
+  EMPTY_NAME_MSG: "⚠️ Please enter a project name",
 };
 
 export const MODAL_BUTTONS = {
